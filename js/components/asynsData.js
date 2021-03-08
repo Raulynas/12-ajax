6a4f6777b9f07c7a094bfbe0053793009f2b79fc
raulynas <laurynas@inwoodengineering.co.uk>
2021-03-08 13:15:48 +0100

    asyncData

---
+++ js/components/asynsData.js
@@ -0,0 +1,6 @@
+function asyncData(httpMethod, url, callback) {
+	fetch(url)
+		.then(response => response.json())
+		.then(data => callback(data));
+}
+export { asyncData };
--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { ajax } from './components/ajax.js';
+import { asyncData } from './components/asyncData.js';
 import { people } from './components/people.js';
 
 const url = 'https://raulynas.github.io/12-ajax/api/users.json';
-ajax('GET', url, people);
+// ajax('GET', url, people);
+asyncData('GET', url, people);
