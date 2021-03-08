8594b4c1eaebd9be87d6bc8033cc182fb8c9aacd
raulynas <laurynas@inwoodengineering.co.uk>
2021-03-08 12:39:39 +0100

    Request data and render

--- js/components/ajax.js
+++ js/components/ajax.js
@@ -1,4 +1,4 @@
-function ajax(httpMethod, url) {
+function ajax(httpMethod, url, callback) {
 	const allowMethods = [
 		'GET', // gauti informacija
 		'POST', //sukurti
@@ -15,6 +15,20 @@ function ajax(httpMethod, url) {
 		console.warn('Netinkama nuoroda');
 		return false;
 	}
+
+	const xhttp = new XMLHttpRequest();
+	xhttp.onreadystatechange = function () {
+		if (this.readyState == 4 && this.status == 200) {
+			try {
+				const parseData = JSON.parse(xhttp.responseText);
+				callback(parseData);
+			} catch (error) {
+				console.error('Gautas nevalidus JSON is serverio', error);
+			}
+		}
+	};
+	xhttp.open(httpMethod, url, true); // if true, user can keep searching while downloading
+	xhttp.send();
 
 	return true;
 }
--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { ajax } from './components/ajax.js';
+import { people } from './components/people.js';
 
-ajax('GET');
+const url = 'https://raulynas.github.io/12-ajax/api/users.json';
+ajax('GET', url, people);
